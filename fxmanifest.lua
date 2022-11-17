5e1505c426b415d733e908b398681da2dfaa6ef0
MobScripts <113401404+MobScripts@users.noreply.github.com>
2022-11-17 23:30:45 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'V3CT0RBUG'
+description 'Mob PauseSystem by V3CT0RBUG'
+version '1.0'
+
+server_script 'main.lua'
+
+pause_updater 'yes'
+pause_name '^2Mob PauseSystem'
+pause_github 'https://github.com/MobScript/mob_pausesystem'
+
